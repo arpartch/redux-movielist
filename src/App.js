8daf2f536edcc530b41cbd32eeb0b724f7919957
Athena <athena.partch@gmail.com>
2021-04-22 17:20:02 -0700

    add props to store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import {connect} from 'react-redux';
+import updateMovies from './store/actions/updateMovies';
 
-function App() {
+function App(props) {
   return(
     <div className="App">
-      REDUX MOVIELIST APP
+      <h3>ATHENA'S REDUX MOVIELIST APP</h3>
+      <br/>
+      <span
+      style={{color: 'limegreen'}}>
+        YOUR CURRENT MOVIE IS:
+      </span>
+      {props.movies.name}
+      <br/>
+      <button onClick={props.updateMovies}>SELECT NEW MOVIE</button>
     </div>
   );
 };
 
-export default App;
+const MapStateToProps = (state) => {
+  return {
+    movies: state.movies
+  };
+};
+
+const MapDispatchToProps = (dispatch) => {
+  return {
+    updateMovies: ()=> dispatch(updateMovies)
+  }
+};
+
+export default connect(MapStateToProps, MapDispatchToProps)(App);
